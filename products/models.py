5b96e6ddb9aabfdba8295234de49123960e09f38
juliachelsie <juliachelsiegustafsson@gmail.com>
2024-03-07 19:05:35 +0000

    Added products details

--- products/models.py
+++ products/models.py
@@ -8,10 +8,10 @@ class Product(models.Model):
     description = models.TextField()
     sku = models.CharField(max_length=200, null=True, blank=True)
     rating = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_url = models.URLField(max_length=1000, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-
+    product_id = models.IntegerField(null=True, blank=True)
     def __str__(self):
         return self.name
 
