42daddb9fe9c0bf37fbad006d1e62c68f8a4cf77
juliachelsie <juliachelsiegustafsson@gmail.com>
2024-03-12 10:55:56 +0000

    Adding sorting via rating, price and category

--- products/views.py
+++ products/views.py
@@ -11,27 +11,43 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories= None
+    sort = None
+    direction = None
     
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'l_name'
+                products = products.annotate(l_name=L('name'))
+
+            if 'direction' in request.GET:
+                direction= request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category']
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    present_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_box': query,
         'present_categories': categories,
+        'present_sorting' : present_sorting
     }
 
     return render(request, 'products/products.html', context)
