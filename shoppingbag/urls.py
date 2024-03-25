09db1b82f3f1838e6db9be95dbf1be914193ec37
juliachelsie <juliachelsiegustafsson@gmail.com>
2024-03-25 14:24:21 +0000

    Added functionality that adds to bag

--- shoppingbag/urls.py
+++ shoppingbag/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_shoppingbag, name='view_shoppingbag')
+    path('', views.view_shoppingbag, name='view_shoppingbag'),
+    path('add/<item_id>/', views.add_bag, name='add_bag'),
 ]
--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,18 @@ def view_shoppingbag(request):
     """ A view that renders the shoppingbag content page """
     
     return render(request, 'shoppingbag/shoppingbag.html')
+
+def add_bag(request, item_id):
+    """ A view to add a quantity of a specified product to shopping bag """
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    shoppingbag = request.session.get('shoppingbag', {})
+
+    if item_id in list(shoppingbag.keys()):
+        shoppingbag[item_id] += quantity
+    else:
+        shoppingbag[item_id] = quantity
+
+    request.session['shoppingbag'] = shoppingbag
+    print(request.session['shoppingbag'])
+    return redirect(redirect_url)
