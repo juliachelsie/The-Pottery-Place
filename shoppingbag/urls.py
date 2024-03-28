73dd42ea52061a406314c2eef5ccaeb3ca4c9c3e
juliachelsie <juliachelsiegustafsson@gmail.com>
2024-03-28 15:08:56 +0000

    Added toasts

--- shoppingbag/urls.py
+++ shoppingbag/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.view_shoppingbag, name='view_shoppingbag'),
     path('add/<item_id>/', views.add_bag, name='add_bag'),
+    path('modify/<item_id>/', views.modify_bag, name='modify_bag'),
+    path('remove/<item_id>/', views.remove_bag, name='remove_bag'),
 ]
--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -9,6 +12,7 @@ def view_shoppingbag(request):
 
 def add_bag(request, item_id):
     """ A view to add a quantity of a specified product to shopping bag """
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     shoppingbag = request.session.get('shoppingbag', {})
@@ -17,6 +21,41 @@ def add_bag(request, item_id):
         shoppingbag[item_id] += quantity
     else:
         shoppingbag[item_id] = quantity
+        messages.success(request, f'Successfully added {product.name} to your bag.')
 
     request.session['shoppingbag'] = shoppingbag
     return redirect(redirect_url)
+
+
+def modify_bag(request, item_id):
+    """ Modify the quantity of a product in the shoppingbag """
+    quantity = int(request.POST.get('quantity'))
+    shoppingbag = request.session.get('shoppingbag', {})
+    
+    if quantity:
+        if quantity > 0:
+            shoppingbag[item_id]=quality
+    else:
+        del bag[item_id]
+        if not shoppingbag[item_id]:
+            shoppingbag.pop(item_id)
+        
+
+    request.session['shoppingbag'] = shoppingbag
+    return redirect(reverse('view_shoppingbag'))
+
+def remove_bag(request, item_id):
+    """ Remove product from the shoppingbag """
+    quantity = int(request.POST.get('quantity'))
+    shoppingbag = request.session.get('shoppingbag', {})
+    
+    try:
+        if quantity > 0:
+            shoppingbag[item_id] = quantity
+        else:
+            shoppingbag.pop[item_id]
+
+        request.session['shoppingbag'] = shoppingbag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
