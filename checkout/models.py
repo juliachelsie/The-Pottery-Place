82869361e3b2616d63883dcbc4cecef20e56bee3
juliachelsie Gustafsson <juliachelsiegustafsson@gmail.com>
2024-04-12 15:07:56 +0000

    Updated allauth templates

--- checkout/models.py
+++ checkout/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 from products.models import Product
-from profiles.models import User_profile
+from profiles.models import UserProfile
 
 from django_countries.fields import CountryField
 
@@ -12,7 +12,7 @@ from django_countries.fields import CountryField
 
 class Order(models.Model):
     order_number = models.CharField(max_length=25, null=False, editable=False)
-    user_profile = models.ForeignKey(User_profile, on_delete=models.SET_NULL, null=True, 
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, 
                                     blank=True, related_name='orders')
     name = models.CharField(max_length=55, null=False, blank=False)
     email = models.EmailField(max_length=240, null=False, blank=False)
--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_countries.fields import CountryField
 
 # Create your models here.
 
-class User_profile(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone = models.CharField(max_length=30, null=True, blank=True)
     default_country = CountryField(blank_label='Country *', blank=True, null=True)
@@ -19,7 +19,7 @@ class User_profile(models.Model):
         return self.user.username
 
 @receiver(post_save, sender=User)
-def create_update_userProfile(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     instance.userprofile.save()
